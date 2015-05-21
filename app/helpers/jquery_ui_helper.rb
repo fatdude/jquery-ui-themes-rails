95b9993bd9b2e039016959aca42097898deb2d7e
Andrea Salicetti <andrea.salicetti@gmail.com>
2015-05-21 09:53:30 +0200

    Linting

--- app/helpers/jquery_ui_helper.rb
+++ app/helpers/jquery_ui_helper.rb
@@ -1,5 +1,5 @@
 module JqueryUiHelper
-  def jqueryui_css_google_cdn(theme, version=nil)
+  def jqueryui_css_google_cdn(theme, version = nil)
     version = JqueryUiThemes::JQUERYUI_VERSION unless version
     "//ajax.googleapis.com/ajax/libs/jqueryui/#{version}/themes/#{theme}/jquery-ui.css"
   end
--- lib/jquery-ui-themes/google_cdn.rb
+++ lib/jquery-ui-themes/google_cdn.rb
@@ -8,29 +8,29 @@ module JqueryUiThemes
     base_uri '//ajax.googleapis.com/ajax/libs/jqueryui/'
 
     class << self
-    
+
       def download(theme, version)
         version ||= JqueryUiThemes::JQUERYUI_VERSION
 
         initial_path = FileUtils.pwd
-        
+
         path = "/#{version}/themes/#{theme}/jquery-ui.css"
 
         css = get(path)
-        
-        if css.success?          
+
+        if css.success?
           FileUtils.mkdir_p(File.expand_path("./app/assets/stylesheets/jquery-ui/#{version}/"))
           FileUtils.mkdir_p(File.expand_path("./app/assets/images/jquery-ui/#{version}/#{theme}/"))
-          
+
           # Store the css file
-          File.open(File.expand_path("./app/assets/stylesheets/jquery-ui/#{version}/#{theme}.css.scss"), "w") do |file| 
+          File.open(File.expand_path("./app/assets/stylesheets/jquery-ui/#{version}/#{theme}.css.scss"), "w") do |file|
             content = css.gsub(/0pxdow=0px/, '0px') # Weird Google CDN bug
             content = content.gsub(/url\(images\/(.*)(\.png|.gif\))/, 'url(image-path(\'jquery-ui/' + version + '/' + theme + '/\1\2\')')
             file.puts(content)
           end
-          
+
           dest_path = File.expand_path("./app/assets/images/jquery-ui/#{version}/#{theme}/")
-          
+
           FileUtils.cd(dest_path)
 
           # Store the images
@@ -46,24 +46,24 @@ module JqueryUiThemes
         else
           puts "Failed to download the css: #{path}"
         end
-        
+
         css
       end
-      
+
       def download_all(version)
         version ||= JqueryUiThemes::JQUERYUI_VERSION
-        
+
         themes.each do |theme|
           self.download(theme, version)
         end
       end
-      
-      private 
-      
+
+      private
+
         def themes
           %w{black-tie blitzer cupertino dark-hive dot-luv eggplant excite-bike flick hot-sneaks humanity le-frog mint-choc overcast pepper-grinder redmond smoothness south-street start sunny swanky-purse trontastic ui-darkness ui-lightness vader}
         end
-    
+
     end
   end
-end 
+end
